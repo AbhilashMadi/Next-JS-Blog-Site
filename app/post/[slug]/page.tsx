1bcb868f01606b856236c8268ce367c9cf27d538
Abhilash Madi <madiabhilash1563@gmail.com>
2023-09-21 23:57:59 +0530

    post page

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { notFound } from "next/navigation";
+
+import { DUMMY_POSTS } from "@temp/collections";
+import { Post } from "@temp/temp.types";
+import Container from "@common/Container";
+
+const generateStaticParams = async () => {
+  return DUMMY_POSTS.map((post:Post) => {
+    return {
+      slug: post.slug,
+    }
+  })
+}
+
+interface IPostPage {
+  params: { slug: string };
+}
+
+const PostPage: FC<IPostPage> = (props) => {
+  const { params: { slug } } = props;
+
+  const post = DUMMY_POSTS.find((post: Post) => post.slug === slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default PostPage;
