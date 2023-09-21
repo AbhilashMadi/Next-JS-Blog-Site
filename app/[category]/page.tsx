a3ce44a1ba5c610a5e5d96de34ef02be913431fd
Abhilash Madi <madiabhilash1563@gmail.com>
2023-09-21 23:12:52 +0530

    category page

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import { notFound } from "next/navigation";
+
 import { DUMMY_POSTS, DUMMY_CATEGORIES } from "@temp/collections";
-import { Post,Category } from "@temp/temp.types";
+import { Post, Category } from "@temp/temp.types";
+import Container from "@common/Container";
+import PostsList from "@post/PostsList";
+import { POST_LAYOUT } from "@lib/enums";
 
 export const generateStaticParams = async () => {
-  return DUMMY_CATEGORIES.map((category:Category) => {
+  return DUMMY_CATEGORIES.map((category: Category) => {
     return {
       category: category.slug,
     };
@@ -18,8 +23,18 @@ const Category: FC<ICategoryPage> = (props) => {
   const { params } = props;
   const { category } = params;
 
+  const posts: Post[] = DUMMY_POSTS.filter((post: Post) => {
+    return post.category.title.toLowerCase() === category;
+  })
+
+  if (!posts.length) {
+    notFound();
+  }
+
   return (
-    <div>{JSON.stringify((DUMMY_POSTS.filter((post: Post) => post.category.title.toLowerCase() === category)))}</div>
+    <Container>
+      <PostsList posts={posts} layout={POST_LAYOUT.VERTICAL} />
+    </Container>
   )
 }
 
